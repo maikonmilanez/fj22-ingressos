fc257e3c12bb073edaa49fe8d40e9ad59d57c8e1
maikon.milanez <maikonmilanez@yahoo.com.br>
2018-02-06 22:51:37 -0200

    Mensagem explicando o trabalho que foi feito

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,54 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Sessao {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private LocalTime horario;
+	
+	@ManyToOne
+	private Sala sala;
+	
+	@ManyToOne
+	private Filme filme;
+	
+	/**
+	*	@deprecated	hibernate	only
+	*/
+	
+	public Sessao(){
+		
+	}
+	
+	public Sessao(LocalTime horario, Filme filme, Sala sala){
+		this.setHorario(horario);
+		this.filme = filme;
+		this.sala = sala;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+}
